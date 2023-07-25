3108e8190c126c70651dc784eaccb0a37a2196ac
soubj <sobujahmed124@gmail.com>
2023-07-25 14:21:22 +0600

    finished about us section

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -12,14 +12,23 @@ const About = () => {
             className="w-1/2 absolute right-5 top-1/2 border-8 border-white rounded-lg shadow-2xl"
           />
         </div>
-        <div className="lg:w-1/2">
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="lg:w-1/2 space-y-5 px-4">
+          <h3 className="text-3xl text-red-500 font-bold">About Us</h3>
+          <h1 className="text-5xl font-bold">
+            We are qualified & of experience in this field
+          </h1>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which do not look even slightly
+            believable.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <p>
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which do not look even slightly
+            believable.
+          </p>
+          <button className="btn bg-rose-600 text-white">Get More Info</button>
         </div>
       </div>
     </div>
