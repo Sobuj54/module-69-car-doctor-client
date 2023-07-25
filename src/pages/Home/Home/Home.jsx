f671e18988edab68bdd5875836d3e2e099943c50
soubj <sobujahmed124@gmail.com>
2023-07-25 14:06:46 +0600

    added img and gave proper style to about us image

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,7 +1,13 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
-  return <Banner></Banner>;
+  return (
+    <>
+      <Banner></Banner>
+      <About></About>
+    </>
+  );
 };
 
 export default Home;
