ff55f2fec4ac89ad2829dec0b69ed337878069cf
soubj <sobujahmed124@gmail.com>
2023-07-25 15:19:20 +0600

    created services and made mobile responsive

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             className="w-1/2 absolute right-5 top-1/2 border-8 border-white rounded-lg shadow-2xl"
           />
         </div>
-        <div className="lg:w-1/2 space-y-5 px-4">
+        <div className="lg:w-1/2 space-y-5 px-4 mt-10 md:mt-0">
           <h3 className="text-3xl text-red-500 font-bold">About Us</h3>
           <h1 className="text-5xl font-bold">
             We are qualified & of experience in this field
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -5,12 +5,12 @@ import img4 from "../../../assets/images/banner/4.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full h-[600px] mt-8">
+    <div className="carousel w-full h-fit md:h-[600px] mt-8">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={img1} className="w-full rounded-xl" />
-        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-          <div className="space-y-7 text-white w-1/2 pl-12">
-            <h2 className="text-6xl font-bold">
+        <div className="absolute rounded-xl flex items-center  md:h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="space-y-2 md:space-y-7 text-white w-11/12 lg:w-1/2 pl-2 md:pl-12">
+            <h2 className="text-2xl md:text-6xl font-bold">
               Affordable Price For Car Servicing
             </h2>
             <p>
