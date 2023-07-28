e21bbecfc223760ba8d424888c3a5006f91a983d
soubj <sobujahmed124@gmail.com>
2023-07-28 16:02:45 +0600

    booking inside private route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -26,7 +26,11 @@ const router = createBrowserRouter([
       },
       {
         path: "checkout/:id",
-        element: <CheckOut></CheckOut>,
+        element: (
+          <PrivateRoute>
+            <CheckOut></CheckOut>
+          </PrivateRoute>
+        ),
         loader: ({ params }) =>
           fetch(
             `https://car-doctor-server-three-chi.vercel.app/services/${params.id}`
