aaa2297efbaecfa87639990b23bdcfd18c175667
soubj <sobujahmed124@gmail.com>
2023-07-28 17:46:13 +0600

    fixed few things

--- src/pages/shared/OrderHero/OrderHero.jsx
+++ src/pages/shared/OrderHero/OrderHero.jsx
@@ -7,7 +7,7 @@ const OrderHero = () => {
         <img src={checkout} alt="Shoes" className="w-full" />
       </figure>
       <div className="card-body relative ">
-        <h2 className="text-xl md:text-4xl font-bold absolute top-auto md:top-1/2 md:ml-12">
+        <h2 className="text-xl md:text-4xl font-bold absolute top-auto md:top-1/2 lg:top-36 ml-4 md:ml-12">
           Booking Details
         </h2>
       </div>
