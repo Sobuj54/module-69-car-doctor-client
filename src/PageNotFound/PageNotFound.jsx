ffebf1d6148f382f8c36345d4f714136ad7c74a6
soubj <sobujahmed124@gmail.com>
2023-07-30 14:22:00 +0600

    fixed few errors and bugs

--- src/PageNotFound/PageNotFound.jsx
+++ src/PageNotFound/PageNotFound.jsx
@@ -3,6 +3,13 @@ const PageNotFound = () => {
     <div className=" text-center h-screen my-auto flex flex-col justify-center">
       <h2 className="text-5xl font-bold text-warning">404</h2>
       <p className="text-xl font-semibold mt-5">Page Not Found</p>
+      <label className="swap swap-flip text-9xl">
+        {/* this hidden checkbox controls the state */}
+        <input type="checkbox" />
+
+        <div className="swap-on">😈</div>
+        <div className="swap-off">😇</div>
+      </label>
     </div>
   );
 };
--- src/pages/shared/NavBar/NavBar.jsx
+++ src/pages/shared/NavBar/NavBar.jsx
@@ -42,33 +42,44 @@ const NavBar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100 h-28 mb-4">
+    <div className="navbar bg-base-100 h-28 mb-4 mt-2 md:mt-5">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <label
+            tabIndex={0}
+            className="btn btn-circle btn-ghost md:hidden swap swap-rotate">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" />
+
+            {/* hamburger icon */}
             <svg
+              className="swap-off fill-current"
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
+              width="32"
+              height="32"
+              viewBox="0 0 512 512">
+              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            </svg>
+
+            {/* close icon */}
+            <svg
+              className="swap-on fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 512 512">
+              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
             </svg>
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            className=" dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             {navItems}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
+        <div>
           <img src={logo} alt="" />
-        </Link>
+        </div>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navItems}</ul>
