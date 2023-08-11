503a4270ae088e7690a81d4d73e2cef1b30460c0
soubj <sobujahmed124@gmail.com>
2023-08-11 19:34:02 +0600

    implemented search

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,16 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
   const [ascending, setAscending] = useState(true);
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch(`http://localhost:3000/services?sort=${ascending ? 1 : -1}`)
+    fetch(
+      `http://localhost:3000/services?search=${search}&sort=${
+        ascending ? 1 : -1
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, [ascending]);
+  }, [ascending, search]);
+
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
 
   return (
     <div className="mt-16">
@@ -22,6 +32,31 @@ const Services = () => {
           humour, or randomised <br /> words which do not look even slightly
           believable.
         </p>
+        <div className="form-control">
+          <div className="input-group flex justify-center">
+            <input
+              type="text"
+              ref={searchRef}
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
         <button
           onClick={() => setAscending(!ascending)}
           className="btn btn-primary">
